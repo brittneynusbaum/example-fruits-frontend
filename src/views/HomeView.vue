1b06040ae4928619df3333b8392e90e3450d64ff
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-07 11:41:57 -0700

    adds index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      fruits: []
+    };
+  },
+  created: function () {
+    this.indexFruits();
+  },
+  methods: {
+    indexFruits: function () {
+      console.log('index fruits: ')
+      axios.get(`/fruits`).then(response => {
+        console.log(response.data);
+        this.fruits = response.data;
+      });
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="fruit in fruits" v-bind:key="fruit.id">
+      <p>{{ fruit.name }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
